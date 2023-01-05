fd7d716796691c35d3c90469436d6f09bce5e6aa
daniel-j-h <daniel@trvx.org>
2023-01-05 22:03:53 +0100

    Makes curl write out errors on failure

--- scrape.sh
+++ scrape.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-curl -sf --proto =https --tlsv1.3 https://www.gesetze-im-internet.de/gii-toc.xml | xmllint --format - > gii-toc.xml
+curl -sSf --proto =https --tlsv1.3 https://www.gesetze-im-internet.de/gii-toc.xml | xmllint --format - > gii-toc.xml
