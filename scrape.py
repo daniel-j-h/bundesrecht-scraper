43eae1b559de80fae06c8302baa1f5cdd515870f
daniel-j-h <daniel@trvx.org>
2023-01-08 22:56:53 +0100

    Implement better request rate limiting

--- scrape.py
+++ scrape.py
@@ -30,6 +30,8 @@ def main():
     tree = ElementTree.parse(tocXmlPath)
     items = list(tree.iter("item"))
 
+    t0 = time.time()
+
     for i, item in enumerate(items):
         title = item.find("title").text
         link = item.find("link").text
@@ -49,14 +51,20 @@ def main():
         outdir = datadir / Path(subdir)
         outdir.mkdir(exist_ok=True)
 
+        t1 = time.time()
+        dt = max(t1 - t0, 0)
+
+        if (dt < 0.1):
+            time.sleep(0.1 - dt)
+
         zipdata = fetch(f"https://{url.netloc}/{subdir}/xml.zip")
+
+        t0 = time.time()
 
         with ZipFile(BytesIO(zipdata)) as zipfile:
             zipfile.extractall(outdir)
 
         print(f"{i+1:4} {len(items):4} {subdir}", file=sys.stderr)
-
-        time.sleep(0.05)
 
 
 def fetch(url, timeout=10):
